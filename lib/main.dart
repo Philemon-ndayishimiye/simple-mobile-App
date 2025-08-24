a8fc6ccf47b5f6b36cb40305a8b0dbeae488c77d
Philemon-ndayishimiye <philemonndayi@gmail.com>
2025-08-24 19:04:20 +0200

    add style for text field

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,87 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        titleTextStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Please Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: "username",
+              labelStyle: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.green, width: 3),
+              ),
+
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey, width: 2),
+              ),
+            ),
+          ),
+
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: "password",
+              labelStyle: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.green, width: 3),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey, width: 2),
+              ),
+            ),
+          ),
+
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7),
+              ),
+            ),
+            onPressed: () => {},
+            child: Text(
+              "Login",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
 }
